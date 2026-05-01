d2aea7e7f4c9ff2ca72a61f7f56decc225dced22
DouglasSouza-cloud <douglassouza4689@gmail.com>
2026-05-01 13:48:37 -0300

    feat: adiciona metricas de producao

--- coleta_dados.py
+++ coleta_dados.py
@@ -7,6 +7,8 @@ st.set_page_config(layout="wide")
 
 st.title("📊 Dashboard Industrial - Sistema de Monitoramento")
 
+META = 250
+
 if "rodando" not in st.session_state:
     st.session_state.rodando = False
     st.session_state.dados = []
@@ -32,5 +34,13 @@ df = pd.DataFrame({
     "Horário": st.session_state.tempos,
     "Produção": st.session_state.dados
 })
+
+media = df["Produção"].mean()
+abaixo_meta = df[df["Produção"] < META]
+
+col1, col2 = st.columns(2)
+
+col1.metric("📊 Média", round(media, 2))
+col2.metric("❌ Abaixo da Meta", len(abaixo_meta))
 
 st.dataframe(df)
