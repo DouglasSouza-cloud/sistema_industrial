c44da8c48d622443a9723c3d7b53546d30339ff6
DouglasSouza-cloud <douglassouza4689@gmail.com>
2026-05-01 13:47:27 -0300

    feat: adiciona exibicao de dados em tabela

--- coleta_dados.py
+++ coleta_dados.py
@@ -1,18 +1,17 @@
 import streamlit as st
 import random
 from datetime import datetime
+import pandas as pd
 
 st.set_page_config(layout="wide")
 
 st.title("📊 Dashboard Industrial - Sistema de Monitoramento")
 
-# 🔥 Estado do sistema
 if "rodando" not in st.session_state:
     st.session_state.rodando = False
     st.session_state.dados = []
     st.session_state.tempos = []
 
-# 🎛 Controles
 colA, colB = st.columns(2)
 
 if colA.button("▶ Iniciar"):
@@ -21,7 +20,6 @@ if colA.button("▶ Iniciar"):
 if colB.button("⛔ Parar"):
     st.session_state.rodando = False
 
-# 🔄 Simulação
 if st.session_state.rodando:
 
     valor = round(random.uniform(100, 400), 2)
@@ -29,3 +27,10 @@ if st.session_state.rodando:
 
     st.session_state.dados.append(valor)
     st.session_state.tempos.append(horario)
+
+df = pd.DataFrame({
+    "Horário": st.session_state.tempos,
+    "Produção": st.session_state.dados
+})
+
+st.dataframe(df)
