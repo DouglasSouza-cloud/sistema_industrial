db0709a19427b2e1b2ea21cb5179a86cafd25e87
DOUGLAS DE SOUZA PAIXÃO <douglas_paixao@estudante.sesisenai.org.br>
2026-05-05 16:17:29 -0300

    feat: versão inicial para deploy

--- coleta_dados.py
+++ coleta_dados.py
@@ -8,14 +8,14 @@ import os
 from dotenv import load_dotenv
 import time
 
-# =========================
-# 🔐 .ENV
-# =========================
+
+# .ENV
+
 load_dotenv()
 
-# =========================
-# 🔌 CONEXÃO
-# =========================
+
+# CONEXÃO
+
 def get_conn():
     return mysql.connector.connect(
         host=os.getenv("DB_HOST"),
@@ -25,17 +25,21 @@ def get_conn():
         port=int(os.getenv("DB_PORT"))
     )
 
-# =========================
-# ⚙ CONFIG
-# =========================
+
+# ONFIG
+
 st.set_page_config(layout="wide")
 st.title("📊 Dashboard Industrial - Monitoramento")
 
 META = 250
 
-# =========================
-# 🔥 ESTADO
-# =========================
+
+# ESTADO
+
+
+if "tempo_critico" not in st.session_state:
+    st.session_state.tempo_critico = 0
+
 if "dados" not in st.session_state:
     st.session_state.dados = []
     st.session_state.tempos = []
@@ -51,9 +55,9 @@ if col2.button("⛔ Parar"):
 
 placeholder = st.empty()
 
-# =========================
-# 🔄 LOOP TEMPO REAL
-# =========================
+
+# LOOP TEMPO REAL
+
 if st.session_state.rodando:
 
     for i in range(100000):
@@ -64,9 +68,6 @@ if st.session_state.rodando:
         valor = round(random.uniform(100, 400), 2)
         horario = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-        # =========================
-        # 💾 SALVA NO MYSQL
-        # =========================
         try:
             conn = get_conn()
             cursor = conn.cursor()
@@ -78,12 +79,11 @@ if st.session_state.rodando:
             cursor.close()
             conn.close()
 
-        except:
-            pass
+        except Exception as e:
+            st.error(f"Erro no MySQL: {e}")
 
-        # =========================
-        # 📊 DADOS LOCAIS (LIMITADO)
-        # =========================
+        
+        # DADOS LOCAIS (LIMITADO)
         st.session_state.dados.append(valor)
         st.session_state.tempos.append(horario)
 
@@ -99,14 +99,20 @@ if st.session_state.rodando:
         media = df["Produção"].mean()
         ultimo = df["Produção"].iloc[-1]
         abaixo_meta = df[df["Produção"] < META]
+        tempo_critico = len(abaixo_meta)
         eficiencia = (ultimo / META) * 100
+        
+        # TEMPO CRÍTICO
+        if ultimo < META:
+            st.session_state.tempo_critico += 1
+        else:
+            st.session_state.tempo_critico = 0
 
-        # =========================
-        # 🎛 DASHBOARD
-        # =========================
+        
+        # DASHBOARD
         with placeholder.container():
 
-            # 🚨 STATUS
+            # STATUS
             if ultimo < META:
                 st.error("🔴 STATUS: PRODUÇÃO ABAIXO DA META")
             else:
@@ -114,17 +120,18 @@ if st.session_state.rodando:
 
             st.divider()
 
-            # 📊 MÉTRICAS
-            c1, c2, c3, c4 = st.columns(4)
+            # MÉTRICAS
+            c1, c2, c3, c4, c5 = st.columns(5)
 
             c1.metric("📊 Média", round(media, 2))
             c2.metric("📈 Última Produção", ultimo)
             c3.metric("❌ Ocorrências", len(abaixo_meta))
             c4.metric("⚙ Eficiência", f"{eficiencia:.1f}%")
+            c5.metric("⏱ Tempo Crítico", f"{st.session_state.tempo_critico}s")
 
             st.divider()
 
-            # 📈 GRÁFICO
+            # GRÁFICO
             fig = px.line(
                 df,
                 x="Horário",
@@ -159,7 +166,7 @@ if st.session_state.rodando:
 
             st.divider()
 
-            # 🚨 ALERTAS
+            # ALERTAS
             st.subheader("🚨 Alertas")
 
             if not abaixo_meta.empty:
@@ -169,7 +176,7 @@ if st.session_state.rodando:
 
             st.divider()
 
-            # 📋 LOG
+            # LOG
             st.subheader("📋 Log de Ocorrências")
 
             if not abaixo_meta.empty:
